3196703b9a5ff9469b886e2bcca38f29047b5677
“Aniket_D” <aniketdakhare@gmail.com>
2021-06-06 19:01:51 +0530

    [Nilima]Added New Employee

--- Employee-Payroll-Restsharp/EmployeeRestsharpTest/UnitTest1.cs
+++ Employee-Payroll-Restsharp/EmployeeRestsharpTest/UnitTest1.cs
@@ -43,11 +43,38 @@ namespace EmployeeRestsharpTest
             Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
             // Convert the response object to list of employees
             List<Employee> employeeList = JsonConvert.DeserializeObject<List<Employee>>(response.Content);
-            Assert.AreEqual(6, employeeList.Count);
+            Assert.AreEqual(5, employeeList.Count);
             foreach (Employee emp in employeeList)
             {
                 Console.WriteLine("Id: " + emp.Id + "\t" + "Name: " + emp.Name + "\t" + "Salary: " + emp.Salary);
             }
+        }
+
+        //UC2 add new emp details
+        [TestMethod]
+        public void OnCallingPostAPI_ReturnEmployeeObject()
+        {
+            //Arrange
+            ///Initialize the request for POST to add new employee
+            RestRequest request = new RestRequest("Employees", Method.POST);
+            JsonObject jsonObj = new JsonObject();
+            jsonObj.Add("name", "Rohit");
+            jsonObj.Add("salary", "50000");
+            jsonObj.Add("id", "6");
+            ///Added parameters to the request object such as the content-type and attaching the jsonObj with the request
+            request.AddParameter("application/json", jsonObj, ParameterType.RequestBody);
+
+            //Act
+            IRestResponse response = client.Execute(request);
+
+            //Assert
+            //HttpsStatusCode=The values of status codes defines  Https
+            //Https status code
+            Assert.AreEqual(HttpStatusCode.Created, response.StatusCode);
+            Employee employee = JsonConvert.DeserializeObject<Employee>(response.Content);
+            Assert.AreEqual("Rohit", employee.Name);
+            Assert.AreEqual("50000", employee.Salary);
+            Console.WriteLine(response.Content);
         }
     }
 }
